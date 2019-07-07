e0bf5c7a60169141c0708720107dbb0d14779803
IFinners <IFinners@ProtonMail.com>
2019-07-07 17:18:01 +0100

    Add --begin argument and function call

--- tests.py
+++ tests.py
@@ -5,19 +5,42 @@
 import io
 import sys
 import unittest
+import unittest.mock
 from unittest.mock import call, patch
 
 import trackerian
 
 
 class TestParseArguments(unittest.TestCase):
-# User runs trackerian.py from commandline and receives help instructions
+
+    # User runs trackerian.py from commandline and receives help instructions
     def test_no_argument_prints_help(self):
         """Test the response of invoking the program with no arguments."""
         captured_output = io.StringIO()
         sys.stdout = captured_output
-        trackerian.parse_arguments()
+        trackerian.parse_arguments([])
+
         self.assertIn("usage:", captured_output.getvalue())
+
+    # From help finds that --begin TASKNAME is how to start timing a task
+    def test_begin_argument_stores_activity_name_in_return(self):
+        """Test that using begin with keyword results in dict entry for it."""
+        args = trackerian.parse_arguments(['--begin', 'Activity Name'])
+
+        self.assertEqual({'begin': 'Activity Name'}, args)
+
+
+class TestArgumentFunctionCalls(unittest.TestCase):
+
+    # User begins timing a new activity by passing --begin argument
+    @patch('trackerian.parse_arguments')
+    @patch('trackerian.begin_activity')
+    def test_begin_activity_called(self, mocked_parse, mocked_begin):
+        """."""
+        mocked_parse.return_value = {'begin': 'Activity Name'}
+        trackerian.main()
+
+        self.assertTrue(mocked_begin.called)
 
 
 
--- trackerian.py
+++ trackerian.py
@@ -5,20 +5,28 @@
 import argparse
 import sys
 
-def parse_arguments():
+def parse_arguments(args):
     """."""
     parser = argparse.ArgumentParser(description="Commandline Time Tracker")
+    parser.add_argument('-b', '--begin', help='starts timing a task')
 
 
-    if len(sys.argv) == 1:
+    if not args:
         parser.print_help()
         sys.exit
 
 
-    return vars(parser.parse_args())
+    return vars(parser.parse_args(args))
 
 
+def begin_activity():
+    """."""
+    pass
+
+def main():
+    """."""
+    args = parse_arguments(sys.argv[1:])
 
 
-
-parse_arguments()
+if __name__ == '__main__':
+    main()
